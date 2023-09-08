50a88de1b331ba536ec7bbaaab160e591b6f6b08
mochfarrel06 <mochfarrel2606@gmail.com>
2023-09-08 17:07:10 +0700

    Menambahkan

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-const elemen = <h1>Hello Word</h1>;
+const elemen = <h1>Hello Wordaa</h1>;
 
 const root = createRoot(document.getElementById('root'));
 root.render(elemen);
